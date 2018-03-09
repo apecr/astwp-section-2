9861080207d596c540a2295fb8db7ea8c0dcc19f
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-03-09 11:38:25 +0100

    doc() complex decorator
    Include a complex decorator
    
    S2,L33

--- decorators.py
+++ decorators.py
@@ -12,4 +12,25 @@ def my_decorator(func):
 def a_function():
     print('I am the function')
 
-a_function()
+# a_function()
+
+##
+
+def decorator_with_arguments(number):
+    def my_decorator(func):
+        @functools.wraps(func)
+        def function_that_runs_func(*args, **kwargs):
+            print('I am the decorator {}'.format(number))
+            if number == 56:
+                print('Not running the function!')
+            else:
+                func(*args, **kwargs)
+            print('After the decorator')
+        return function_that_runs_func
+    return my_decorator
+
+@decorator_with_arguments(42)
+def my_function_too(x, y):
+    print(x, y)
+
+my_function_too(57,67)
